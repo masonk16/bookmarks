80d2bf5af43c90e38ed2c9cf57e15a2855ec338b
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-03-25 21:54:25 +0200

    User Registration Form.

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -140,4 +140,5 @@ LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 
+# TODO: Set production email SMTP
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
