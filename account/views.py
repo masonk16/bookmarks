16b23ad3269cdfe06b066cd40f94310c4120cfe7
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-03-25 23:09:15 +0200

    Add messages to edit view and template.

--- account/views.py
+++ account/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
@@ -65,6 +66,9 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Error updating your profile')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
