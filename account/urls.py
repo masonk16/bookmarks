d32d73fd0e31c81db566d8fb568afd38a72b3a08
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-03-25 22:01:38 +0200

    User Registration URL.

--- account/urls.py
+++ account/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
+    path('register/', views.register, name='register'),
 
     # # change password urls
     # path('password_change/', auth_views.PasswordChangeView.as_view(),
