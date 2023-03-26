2cb8ac1d95409607ede4b4de75db285e7a5c7cf6
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-03-26 21:00:36 +0200

    Social Authentication settings

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "social_django",
 ]
 
 MIDDLEWARE = [
