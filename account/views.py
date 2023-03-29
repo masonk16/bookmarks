25b350640c13f724fd11b1639387c7a8eb30dc45
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-03-29 22:24:08 +0200

    join the Profile table in a single SQL query

--- account/views.py
+++ account/views.py
@@ -46,7 +46,7 @@ def dashboard(request):
                                                        flat=True)
     if following_ids:
         # If user is following others, retrieve only their actions
-        actions = actions.filter(user_id__in=following_ids)
+        actions = actions.select_related('user', 'user__profile')[:10]
     actions = actions[:10]
     return render(request, 'account/dashboard.html',
                   {'section': 'dashboard', 'actions': actions})
